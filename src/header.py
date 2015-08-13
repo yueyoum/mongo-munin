f859362d9a7aa1a5266e675dbae2bec332f206ef
yueyoum <yueyoum@gmail.com>
2015-08-13 11:05:58 +0800

    fix port

--- src/header.py
+++ src/header.py
@@ -6,5 +6,6 @@ import pymongo
 def getServerStatus():
     host = os.environ.get("MONGO_HOST", "127.0.0.1")
     port = os.environ.get("MONGO_PORT", 27017)
+    port = int(port)
     c = pymongo.MongoClient(host, port)
     return c.admin.command('serverStatus', workingSet=True)
